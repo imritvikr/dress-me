1c8e1c4f718c51db4115d29b7ca2c28334449804
Ramakant <2016meb1140@iitrpr.ac.in>
2020-08-17 13:04:37 +0530

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51HH2FxC6MpwLAJXCn0GOdH2JPyfH5lEFcWHtliqMnDxnf5YPE3CBfLjUvtnCv9HknqC6GKHaUxLjFAuiLYR4aCjY00V0hCAGPz';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Succesful!');
+    };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='DressMe Private Limited'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
